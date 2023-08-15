af78ef47ba274aab49b8ed8393d76006cff908bd
sachindu kavishka <cst21017@std.uwu.ac.lk>
2023-08-15 23:20:28 +0530

    srcCommit

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "RideBuddy"
+    host: "sql.freedb.tech",
+    user: "freedb_user_rd",
+    password: "kPnX$v7E7PtdGxX",
+    database: "freedb_ridebuddy_db"
 });
 
 module.exports = conn;
