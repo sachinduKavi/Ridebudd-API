3856b7acb6f5dd5124978861ed1941160e0d3050
sachindu kavishka <cst21017@std.uwu.ac.lk>
2023-08-21 17:06:29 +0530

    banner

--- src/controllers/banners.js
+++ src/controllers/banners.js
@@ -1,7 +1,7 @@
 const conn = require('../db');
 
 const getBanners = (req, res) => {
-    conn.query(`SELECT * FROM banners`, function(err, result, fields) {
+    conn.query(`SELECT bannerLink FROM banners`, function(err, result, fields) {
         if (err) console.log(err);
         res.status(200).json({
             data: result
