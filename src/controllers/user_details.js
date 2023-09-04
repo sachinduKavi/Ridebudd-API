8c52eabd0422a55d7a980e2fb01041cb37200c42
sachindu kavishka <cst21017@std.uwu.ac.lk>
2023-09-04 11:33:21 +0530

    add new user commitment

--- src/controllers/user_details.js
+++ src/controllers/user_details.js
@@ -14,8 +14,33 @@ const getUserDetails = (req, res) => {
     });
 }
 
+const newUser = (req, res) => {
+    console.log("New User");
+    const fName = req.params.fName;
+    const lName = req.params.LName;
+    const userEmail = req.params.userEmail;
+    const password = req.params.pass;
+    const mobileNumber = req.params.mobile;
+    //  Fetching last userID from database
+    conn.query("SELECT userID FROM user_details ORDER BY userID DESC LIMIT 1", function(err, result, fields) {
+        console.log("LastID: ", result);
+        var userNewID = "UA" + (parseInt(result[0]["userID"].substring(2)) + 1).toString().padStart(6, '0');
+        console.log("New UserID " ,userNewID);
+
+        conn.query(`INSERT INTO user_details(userID, f_name, l_name, user_email, password, mobileNumber, dp_image) VALUES('${userNewID}', '${fName}','${lName}','${userEmail}','${password}','${mobileNumber}',
+        'default')`, function(err, result, fields) {
+            if(err) throw err;
+            res.status(201).json({
+                message: "Record Created",
+                status: result
+            });
+        });
+    }); 
+}
+
 
 
 module.exports = {
-    getUserDetails
+    getUserDetails,
+    newUser
 }
--- src/routes/user_details.js
+++ src/routes/user_details.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { getUserDetails } = require('../controllers/user_details');
+const { getUserDetails, newUser } = require('../controllers/user_details');
 
 
 router.get('/getUserDetails/:userID/:userEmail', getUserDetails);
+
+router.post('/newUser/:fName/:LName/:userEmail/:pass/:mobile', newUser);
 
 
 module.exports = router;
