6332572a5d6faec859ca2ad0cc10c6a2eae4a7fb
sachindu kavishka <cst21017@std.uwu.ac.lk>
2023-09-04 11:57:47 +0530

    myComitment

--- src/controllers/user_details.js
+++ src/controllers/user_details.js
@@ -5,7 +5,7 @@ const conn = require('../db');
 const getUserDetails = (req, res) => {
     console.log("Get User Details");
     const userID = req.params.userID;
-    const userEmail = req.params.userEmail;
+    const userEmail = req.params.userEmail.replace('*', '.');
     
     conn.query(`SELECT * FROM user_details WHERE userID='${userID}' OR user_email='${userEmail}'`, function(err, result, fields) {
         res.status(200).json({
